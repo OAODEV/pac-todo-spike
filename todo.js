99345fe59ceda89ed75cb1d06c09345dd0b080da
Jesse Miller <jesse.miller@adops.com>
2015-09-10 11:08:07 -0500

    implements addTodo Agent

--- todo.js
+++ todo.js
@@ -1,26 +1,57 @@
 $(document).ready(function () {
-    app.view($("#root"));
+    var app = runApp($("#root"));
 });
 
-var app = (function () {
+var runApp = function (rootNode) {
     var self = this;
 
     self.model = {
-        "message": "this is the app",
-        "rootNode": null,
+        "title": "PAC Todo",
     }
 
-    self.view = function(rootNode) {
-        if (rootNode) {
-            self.model.rootNode = rootNode;
-        }
-        self.model.rootNode.html("<h2>" + self.model.message + "</h2>");
+    self.view = function() {
+        rootNode.html("<h2>" + self.model.message + "</h2>");
     }
 
-    self.setMessage = function(newMessage) {
-        self.model.message = newMessage
+    self.setTitle = function(newTitle) {
+        self.model.title = newTitle
         self.view()
     }
 
     return self;
-}());
+};
+
+var addItem = function (rootNode, text) {
+    var self = this;
+
+    /* abstraction */
+    self.model = {
+        "text": text,
+        "done": false,
+    }
+
+    /* presentation */
+    self.view = function () {
+        rootNode.html(self.model.text);
+        if (!done) {
+            rootNode.removeClass("done");
+            rootNode.addClass("todo");
+        } else {
+            rootNode.removeClass("todo");
+            rootNode.addClass("done");
+        }
+    };
+
+    /* control */
+    self.done = function () {
+        self.model.done = true;
+        self.view();
+    }
+
+    self.todo = function () {
+        self.model.done = false;
+        self.view();
+    }
+
+    return self;
+};
